297f859805634e72a7781cb11df414a251370e32
shizhiyoubajiu <1458842209@qq.com>
2019-07-02 21:44:35 +0800

    goodes

---
+++ shop/src/components/NavBread.vue
@@ -0,0 +1,16 @@
+<template>
+ <div class="NavBread">
+ <nav class="nav-breadcrumb">
+      <slot name="bread"></slot>
+</nav>
+ </div>
+</template>
+ 
+<script>
+export default {
+}
+</script>
+ 
+<style scoped >
+ 
+</style>
